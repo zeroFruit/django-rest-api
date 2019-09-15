bb0192364f3a27e50e0141be1079278429865576
zerofruit <14wnrkim@gmail.com>
2019-09-15 15:32:52 +0900

    add uesr management endpoint

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from user.views import CreateUserView, CreateTokenView
+from user.views import CreateUserView, CreateTokenView, ManageUserView
 
 
 app_name = 'user'
 
 urlpatterns = [
     path('', CreateUserView.as_view(), name='create'),
-    path('token', CreateTokenView.as_view(), name='token')
+    path('token', CreateTokenView.as_view(), name='token'),
+    path('me', ManageUserView.as_view(), name='me')
 ]
